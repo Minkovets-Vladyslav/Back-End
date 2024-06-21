14ee7b40255bc6a1e2c898ee1b2e17e37937ed6f
Vladuslav Minkovets <minkovprogram@gmail.com>
2024-06-21 19:09:15 +0300

    .

--- lab1/index.php
+++ lab1/index.php
@@ -1,85 +1,137 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Лабораторна 1</title>
-    </head>
-    <body>
-        <h3>Завдання 2</h3>
-            <?php
-            echo "Полину в мріях в купель океану,<br>
+<head>
+    <title>Лабораторна 1</title>
+    <style>
+        .accordion {
+            background-color: #eee;
+            color: #444;
+            cursor: pointer;
+            padding: 18px;
+            width: 100%;
+            text-align: left;
+            border: none;
+            outline: none;
+            transition: 0.4s;
+        }
+
+        .active, .accordion:hover {
+            background-color: #ccc;
+        }
+
+        .panel {
+            padding: 0 18px;
+            display: none;
+            background-color: white;
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+<button class="accordion">Завдання 2</button>
+<div class="panel">
+    <?php
+    echo "Полину в мріях в купель океану,<br>
                       Відчую <b>шовковистість</b> глибини,<br>
                       Чарівні мушлі з дна собі дістану,<br>
                       Щоб <i><b>взимку</b></i>   <br>
                       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>тішили</u><br>
                       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;мене<br>
                       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;вони…<br>";
-            ?>
-        <h3>Завдання 3</h3>
-            <?php
-            $uah = 1500;
-            $exchangeRate = 29.41;
-            $usd = $uah / $exchangeRate;
-            echo "$uah грн. можна обміняти на " . round($usd, 2) . " долар";
-            ?>
-        <h3>Завдання 4</h3>
-            <?php
-            $month = 5;
+    ?>
+</div>
+<button class="accordion">Завдання 3</button>
+<div class="panel">
+    <?php
+    $uah = 1500;
+    $exchangeRate = 29.41;
+    $usd = $uah / $exchangeRate;
+    echo "$uah грн. можна обміняти на " . round($usd, 2) . " долар";
+    ?>
+</div>
+<button class="accordion">Завдання 4</button>
+<div class="panel">
+    <?php
+    $month = 5;
 
-            if ($month == 12 || $month == 1 || $month == 2) {
-                echo "Зима";
-            } elseif ($month >= 3 && $month <= 5) {
-                echo "Весна";
-            } elseif ($month >= 6 && $month <= 8) {
-                echo "Літо";
-            } elseif ($month >= 9 && $month <= 11) {
-                echo "Осінь";
-            } else {
-                echo "Неправильний номер місяця";
-            }
-            ?>
-        <h3>Завдання 5</h3>
-            <?php
-            $char = 'a';
+    if ($month == 12 || $month == 1 || $month == 2) {
+        echo "Зима";
+    } elseif ($month >= 3 && $month <= 5) {
+        echo "Весна";
+    } elseif ($month >= 6 && $month <= 8) {
+        echo "Літо";
+    } elseif ($month >= 9 && $month <= 11) {
+        echo "Осінь";
+    } else {
+        echo "Неправильний номер місяця";
+    }
+    ?>
+</div>
+<button class="accordion">Завдання 5</button>
+<div class="panel">
+    <?php
+    $char = 'a';
 
-            switch ($char) {
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u':
-                case 'A':
-                case 'E':
-                case 'I':
-                case 'O':
-                case 'U':
-                    echo "$char є голосною";
-                    break;
-                default:
-                    echo "$char є приголосною";
-                    break;
-            }
-            ?>
-        <h3>Завдання 6</h3>
-            <?php
-            $number = mt_rand(100, 999);
-            echo "Число: $number<br>";
+    switch ($char) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            echo "$char є голосною";
+            break;
+        default:
+            echo "$char є приголосною";
+            break;
+    }
+    ?>
+</div>
+<button class="accordion">Завдання 6</button>
+<div class="panel">
+    <?php
+    $number = mt_rand(100, 999);
+    echo "Число: $number<br>";
 
-            $digits = str_split($number);
-            $sum = array_sum($digits);
-            echo "Сума цифр: $sum<br>";
+    $digits = str_split($number);
+    $sum = array_sum($digits);
+    echo "Сума цифр: $sum<br>";
 
-            $reversed = implode('', array_reverse($digits));
-            echo "Зворотній порядок: $reversed<br>";
+    $reversed = implode('', array_reverse($digits));
+    echo "Зворотній порядок: $reversed<br>";
 
-            rsort($digits);
-            $maxNumber = implode('', $digits);
-            echo "Найбільше число: $maxNumber<br>";
-            ?>
-        <h3>Завдання 7.1</h3>
-        <style>
-            table { border-collapse: collapse; }
-            td { width: 50px; height: 50px; }
-        </style>
+    rsort($digits);
+    $maxNumber = implode('', $digits);
+    echo "Найбільше число: $maxNumber<br>";
+    ?>
+</div>
+<button class="accordion">Завдання 7</button>
+<div class="panel">
+    
+    <style>
+        .row {
+            display: flex;
+        }
+
+        .column {
+            flex: 50%;
+        }
+        canvas {
+            background-color: black;
+        }
+    </style>
+    <div class="row">
+        <div class="column">
+
+            <h3>Завдання 7.1</h3>
+            <style>
+                table { border-collapse: collapse; }
+                td { width: 50px; height: 50px; }
+            </style>
             <?php
             function generateColorTable($rows, $cols) {
                 echo "<table>";
@@ -96,12 +148,11 @@
 
             generateColorTable(5, 5);
             ?>
-        <h3>Завдання 7.2</h3>
-            <style>
-                canvas {
-                    background-color: black;
-                }
-            </style>
+        </div>
+
+        <div class="column">
+
+            <h3>Завдання 7.2</h3>
             <canvas id="myCanvas" width="400" height="400"></canvas>
             <script>
                 function getRandomInt(min, max) {
@@ -128,5 +179,26 @@
                     ?>
                 };
             </script>
-    </body>
+        </div>
+    </div>
+    
+    
+</div>
+<script>
+    let acc = document.getElementsByClassName("accordion");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+</script>
+</body>
 </html>
--- lab3/index.php
+++ lab3/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab3</title>
+    <title>Лабораторна 3</title>
     <style>
         .accordion {
             background-color: #eee;
